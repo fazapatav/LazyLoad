961f2fb7537afbb9abd97a6d9c123935004baa52
Fabian Zapata <fazapatav@gmail.com>
2017-10-09 22:33:16 -0500

    se crea Ejecución Dto y CasosServices

---
+++ LazyLoad/LazyLoad.Api/Controllers/EjecucionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace LazyLoad.Api.Controllers
+{
+    public class EjecucionController : ApiController
+    {
+        #region Member
+        #endregion Member
+        #region Constructor
+        #endregion
+        #region Methods
+        #endregion
+
+    }
+}
